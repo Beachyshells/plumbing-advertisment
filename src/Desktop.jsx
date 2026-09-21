4139e1d35158289c799534324f57c9925b9a4964
Michelle Durham <5beachflower5@gmail.com>
2026-09-21 14:06:19 -0400

    changed the color of the keys

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -990,7 +990,7 @@ function PropertyDetail({ property: initialProperty, onBack, onViewCustomer }) {
                                 <div>
                                     <p className="text-white text-base font-serif">{item.equipmentType || 'Untitled unit'}</p>
                                     {(item.make || item.model) && (
-                                        <p className="text-white/50 text-xs mt-0.5">
+                                        <p className="text-light-blue text-xs mt-0.5">
                                             {[item.make ? `Make: ${item.make}` : null, item.model ? `Model: ${item.model}` : null].filter(Boolean).join(' · ')}
                                         </p>
                                     )}

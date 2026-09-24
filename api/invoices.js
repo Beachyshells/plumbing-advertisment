7d5096e38473969701ab1237b2e9b24182098c47
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2026-09-24 18:40:09 -0400

    Update invoices.js

--- api/invoices.js
+++ api/invoices.js
@@ -293,7 +293,7 @@ export default async function handler(req, res) {
                         discountAppliedBy,
                         confirmationEmailSent,
                         confirmationEmailSentAt,
-"customerId": customer_id,
+"customerId": customer->_id,
                         "propertyId": property->_id,
                         "propertyAddress": property->address,
                         "customerFirstName": customer->firstName,
